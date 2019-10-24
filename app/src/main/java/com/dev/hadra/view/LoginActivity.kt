6c9da18cddeed77a47089fd364cf34d76a0d1e5c
user <user@MacBook-Pro-de-user.local>
2019-10-24 02:43:51 +0100

    Signin/Signup with firebase without design

---
+++ app/src/main/java/com/dev/hadra/view/LoginActivity.kt
@@ -0,0 +1,89 @@
+package com.dev.hadra.view
+
+import android.widget.Toast
+import android.util.Log
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.hadra.MainActivity
+import com.dev.hadra.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        //Going to signup Activity and killing the login Activity
+        activity_login_btn_toSignup.setOnClickListener{
+            startActivity(Intent(this,SignupActivity::class.java))
+            finish()
+        }
+
+        activity_login_btn_login.setOnClickListener{
+            login()
+        }
+
+
+    }
+
+    public fun login(){
+
+        if (activity_login_et_email.text.toString().isEmpty()) {
+            activity_login_et_email.error = "Email is missing !"
+            activity_login_et_email.requestFocus()
+            return
+        }
+        if (activity_login_et_password.text.toString().isEmpty()) {
+            activity_login_et_password.error = "Email is missing !"
+            activity_login_et_password.requestFocus()
+            return
+        }
+        auth.signInWithEmailAndPassword(activity_login_et_email.text.toString(),activity_login_et_password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    startActivity(Intent(this,MainActivity::class.java))
+                    finish()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        //updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser : FirebaseUser?){
+        if (currentUser != null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+
+        }else{
+            Toast.makeText(baseContext, "Authentication failed.",
+                Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
---
+++ app/src/main/java/com/dev/hadra/view/SignupActivity.kt
@@ -0,0 +1,66 @@
+package com.dev.hadra.view
+
+import android.util.Log
+import android.content.Intent
+import com.dev.hadra.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        activity_signup_btn_signup.setOnClickListener{
+            signupUser()
+        }
+    }
+
+    fun signupUser(){
+        if (activity_signup_et_username.text.toString().isEmpty()) {
+            activity_signup_et_username.error = "Username is missing !"
+            activity_signup_et_username.requestFocus()
+            return
+        }
+        if (activity_signup_et_email.text.toString().isEmpty()) {
+            activity_signup_et_email.error = "Email is missing !"
+            activity_signup_et_email.requestFocus()
+            return
+        }
+        if (activity_signup_et_password.text.toString().isEmpty()) {
+            activity_signup_et_password.error = "Password is missing !"
+            activity_signup_et_password.requestFocus()
+            return
+        }
+        if (activity_signup_et_password.text.toString().length < 6) {
+            activity_signup_et_password.error = "Password should be at least 6 characters"
+            activity_signup_et_password.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(activity_signup_et_email.text.toString(), activity_signup_et_password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    startActivity(Intent(this,LoginActivity::class.java))
+                    finish()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+}
