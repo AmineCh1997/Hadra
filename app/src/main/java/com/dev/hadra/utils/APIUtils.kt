75f71e5b9fd1dca73b7b93b51db63f4b0f5d1ad1
selimmattar <selim.mattar@esprit.tn>
2019-11-04 14:54:13 +0100

    base url fix

--- app/src/main/java/com/dev/hadra/utils/APIUtils.kt
+++ app/src/main/java/com/dev/hadra/utils/APIUtils.kt
@@ -6,7 +6,7 @@ import com.dev.hadra.service.RetrofitClient
 class APIUtils {
 
 companion object {
-    val BASE_URL_CCC = "hadratn.herokuapp.com/"
+    val BASE_URL_CCC = "https://hadratn.herokuapp.com/"
     fun webService(): APIService {
         return RetrofitClient.create(BASE_URL_CCC)
     }
